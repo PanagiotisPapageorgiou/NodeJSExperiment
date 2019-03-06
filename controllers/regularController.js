f6d441c9d891ef11d344629ba5515a7a13a30fa5
PanagiotisPapageorgiou <panopapageorgiou@yahoo.gr>
2019-03-06 21:29:47 +0200

    Completed regular_classic_get and regular_classic_post

--- controllers/regularController.js
+++ controllers/regularController.js
@@ -4,41 +4,70 @@ var os = require('os');
 var exec = require('child_process').exec;
 var child;
 
-// Display Regular Classic GET Form Page
-exports.classic_get = function(req, res, next) {
-  addr = req.query.addr;
-  if (addr === undefined) {    
-    res.render('regular_classic_get', { title: 'Regular Classic GET Form' });
+function ping_an_address(res, addr, template_name, template_title) {
+
+  if (addr === undefined) {
+    console.log('Page load for ' + template_name);    
+    return res.render(template_name, { title: template_title });
   } else {
+    console.log('User submitted addr: |' + addr + '|');
     console.log(os.type()); // "Windows_NT"
     console.log(os.release()); // "10.0.14393"
     console.log(os.platform()); // "win32"
     
     var exec_res = 'Failed to Run...';
-    child = exec('/bin/ping -c 4 ' + addr,
+    var ping_command = '/bin/ping -c 4 ';
+
+    if (os.type().includes('Windows NT')) {
+        ping_command = 'ping ';
+    }
+
+    child = exec(ping_command + addr,
         function (error, stdout, stderr) {
-            console.log('stdout: ' + stdout);
-            console.log('stderr: ' + stderr);
-
-            exec_res = stdout;
-
-            if (error !== null) {
-                console.log('exec error: ' + error);
-                exec_res = error;
+            if (error) {
+                console.log(template_name + ' exec error: ' + error);
+                return res.render(template_name, { title: template_title });
             }
-            res.render('regular_classic_get', { 
-                title: 'Regular Classic GET Form', 
-                ping_res: 'You submitted: ' + addr, 
-                server_os: os.platform(),
-                exec_res: exec_res
-            });
+
+            console.log('stdout: |' + stdout + '|');
+            console.log('stderr: |' + stderr + '|');
+
+            exec_res = null;
+
+            if (stdout.includes('\n')) {
+                var lines = stdout.split('\n');
+                for (var i = 0; i < lines.length; i++) {
+                    if (lines[i].includes('rtt min/avg/max/mdev =')) {
+                        exec_res = lines[i];
+                        break;
+                    }
+                }
+            }
+            
+            if (exec_res == null) {
+                console.log(template_name + ' Failed to locate of average results of ping!')
+                return res.render(template_name, { title: template_title });
+            }
+            else {
+                return res.render(template_name, { 
+                    title: template_title,
+                    exec_res: exec_res
+                });
+            }
         });
   }
+}
 
+exports.classic_get = function(req, res, next) {
+  addr = req.query.addr;
+  return ping_an_address(res, addr, 'regular_classic_get', 'Regular Classic GET Form'); 
 };
 
-// Handle Regular Classic on GET request.
-exports.handle_classic_get = function(req, res, next) {
-    addr = req.body.addr;
-    console.log('Submitted Addr is: ' + addr);
+exports.classic_post = function(req, res, next) {
+  if (req.method === 'GET') { // Form fetching
+    return res.render('regular_classic_post', { title: 'Regular Classic POST Form' });
+  } else { // Form submitting
+      addr = req.body.addr;
+      return ping_an_address(res, addr, 'regular_classic_post', 'Regular Classic POST Form');
+  }
 };
--- routes/regular.js
+++ routes/regular.js
@@ -7,4 +7,8 @@ var regular_controller = require('../controllers/regularController');
 // Classic Regular Example - GET
 router.get('/GET/classic', regular_controller.classic_get);
 
+// Classic Regular Example - POST Form
+router.get('/POST/classic', regular_controller.classic_post);
+router.post('/POST/classic', regular_controller.classic_post);
+
 module.exports = router;
